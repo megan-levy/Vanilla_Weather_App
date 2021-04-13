646f297ba41e482151fb881980f55d5ba78f8c7d
megan levy <69728544+megan-levy@users.noreply.github.com>
2021-04-13 08:18:04 -0400

    JS functions reworked to prepare for later

--- src/index.js
+++ src/index.js
@@ -5,27 +5,29 @@ function displayCurrentTemperature(response) {
     let windSpeed = document.querySelector("#windSpeed");
     let humidity = document.querySelector("#precipitation");
     let description = document.querySelector("#description");
+    let currentLocation = document.querySelector("#city");
 
     let iconElement = document.querySelector("#icon");
     iconElement.setAttribute ("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 
+    currentLocation.innerHTML = `${response.data.name}`;
     windSpeed.innerHTML = `${response.data.wind.speed} MPH`;
     humidity.innerHTML = `${response.data.main.humidity}%`;
     currentTemperature.innerHTML = `${Math.round(response.data.main.temp)}`;
     description.innerHTML = `${response.data.weather[0].description}`;
 
 }
-
-function displayLocation(event) {
-    event.preventDefault();
-    let currentLocation = document.querySelector("#city");
-    let formInput = document.querySelector("#inputLocation");
-    currentLocation.innerHTML = `${formInput.value}`;
+function search(city) {
     let apiKey = "4a89eb9a057b7d42b2048718c9361f4a";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${formInput.value}&appid=${apiKey}&units=imperial`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
     axios.get(apiUrl).then(displayCurrentTemperature);
 }
 
+function handleSubmit(event) {
+    event.preventDefault();
+    let formInput = document.querySelector("#inputLocation");
+    search(formInput.value);
+}
 
 let now = new Date();
 let hour = now.getHours();
@@ -50,7 +52,7 @@ dateTime.innerHTML = `Last updated on ${day} at ${hour}:${minute}`;
 
 
 let form = document.querySelector("#searchEngine");
-form.addEventListener("submit", displayLocation);
+form.addEventListener("submit", handleSubmit);
 
 function showCelsiusConversion(event){
     event.preventDefault();
@@ -72,3 +74,5 @@ farenheitLink.addEventListener("click", showCelsiusConversion);
 
 let celsiuslink = document.querySelector("#farenheit");
 celsiuslink.addEventListener("click", showFarenheitConversion);
+
+search("Columbus");
